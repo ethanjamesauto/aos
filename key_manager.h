d0253c7b44a3f24aa82bed4cb0184f9ec05b8a5a
Ethan James <ethanjamesauto@gmail.com>
2021-08-16 17:06:24 -0400

    formatted key manager class

--- key_manager.h
+++ key_manager.h
@@ -2,18 +2,13 @@
 #define BUFFER_SIZE 256
 
 class KeyManager {
-	char buffer[BUFFER_SIZE];
-	int buffer_loc;
-	
-	char key_pressed;
-		
-	public:
-		
-	int chars_left;
-	
-	KeyManager();
-	
-	void update();
-	
-	char getKeyPress();
+    char buffer[BUFFER_SIZE];
+    int buffer_loc;
+    char key_pressed;
+
+   public:
+    int chars_left;
+    KeyManager();
+    void update();
+    char getKeyPress();
 };
